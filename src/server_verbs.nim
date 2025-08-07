a3b7ec3a91cadd87a4ee9cbcad1374e061ddb236
mbourke525 <mitchell.bourke@ausproof.com.au>
2025-08-08 09:38:21 +1000

    .

--- src/server_verbs.nim
+++ src/server_verbs.nim
@@ -8,24 +8,30 @@ macro get*(name: string, body: untyped): untyped =
 
 macro post*(name: string, body: untyped): untyped =
     result = quote do:
-        if req.reqMethod == HttpPost and req.url.path.startsWith(`name`):
+        if req.reqMethod == HttpPost and curSlice(`name`):
             curPath.add(`name`)
             `body`            
 
 macro put*(name: string, body: untyped): untyped =
     result = quote do:
-        if req.reqMethod == HttpPut and req.url.path.startsWith(`name`):
+        if req.reqMethod == HttpPut and curSlice(`name`):
             curPath.add(`name`)
             `body`
 
 macro patch*(name: string, body: untyped): untyped =
     result = quote do:
-        if req.reqMethod == HttpPatch and req.url.path.startsWith(`name`):
+        if req.reqMethod == HttpPatch and curSlice(`name`):
             curPath.add(`name`)
             `body`
 
 macro delete*(name: string, body: untyped): untyped =
     result = quote do:
-        if req.reqMethod == HttpDelete and req.url.path.startsWith(`name`):
+        if req.reqMethod == HttpDelete and curSlice(`name`):
+            curPath.add(`name`)
+            `body`
+
+macro all*(name: string, body: untyped): untyped =
+    result = quote do:
+        if curSlice(`name`):
             curPath.add(`name`)
             `body`
